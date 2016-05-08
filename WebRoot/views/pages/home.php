419e54f46673a30ad846ee0f4350efc25865ddd7
Ro5635 <ro5635@gmail.com>
2016-05-08 09:00:35 +0100

    secure

--- WebRoot/views/pages/home.php
+++ WebRoot/views/pages/home.php
@@ -34,15 +34,7 @@ Name: <input type="text" name="NameOfNewGame" value=""><br>
 </span>
 
 
-  <script>
-  $(function() {
-    $( "#slider" ).slider();
-  });
-  </script>
-</head>
-<body>
- 
-<div id="slider"></div>
+
 
 
 </span>
