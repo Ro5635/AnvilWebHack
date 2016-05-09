6792741fe392a8e5a54ff9a0d5767f6abf583e8d
Ro5635 <Ro5635@users.noreply.github.com>
2016-05-09 21:51:28 +0100

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -9,7 +9,11 @@
     public static function getInstance() {
       if (!isset(self::$instance)) {
         $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+        
+      //as you may notice security is.... weak.
         self::$instance = new PDO('mysql:host=anvilhackdatabase.cnd1uqnrsv3t.eu-west-1.rds.amazonaws.com;dbname=AnvilHackDB', 'robert', '5635258123', $pdo_options);
+      //NB the above details will not beyond the end of AnvilHack 
+        
       }
       return self::$instance;
     }
